2b55d0ef1e253bb36df2969e68bcb784d933a747
jounaidr <jounaid.ruhomaun@clearswift.com>
2020-06-25 01:39:58 +0100

    Initial project setup. Embedded H2 database setup.

---
+++ src/main/java/com/jounaidruhomaun/springtraining/springboottraining/SpringBootTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.jounaidruhomaun.springtraining.springboottraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jounaidruhomaun/springtraining/springboottraining/SpringBootTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jounaidruhomaun.springtraining.springboottraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
